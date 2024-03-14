4707eef4dd89ba2260812d90f4f01bf05cdec858
BobBieNeno <161856266+BobBieNeno@users.noreply.github.com>
2024-03-14 17:40:15 +0700

    Update uplaods.ts

--- api/uplaods.ts
+++ api/uplaods.ts
@@ -2,11 +2,12 @@ import express from "express";
 import multer from "multer";
 import mysql from "mysql";
 import path from "path";
-import {imgPostResp} from "../model/user_post_resp";
+import {UserPostResp, imgPostResp, User} from "../model/user_post_resp";
 
 import { ref, getStorage, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import {initializeApp} from "firebase/app";
-import { conn } from "../dbconn";
+import { conn, queryAsync } from "../dbconn";
+import { json } from "body-parser";
 
 export const router = express.Router();
 
@@ -58,7 +59,7 @@ router.post("/",fileuploads.diskLoader.single("file"),async (req,res)=>{
     });
 });
 
-router.post("/profile",fileuploads.diskLoader.single("file"),async (req,res)=>{
+router.put("/profile",fileuploads.diskLoader.single("file"),async (req,res)=>{
 
     const filename = Math.round(Math.random() * 10000) + ".png";
     const storageRef = ref(storage,"/images/"+filename);
@@ -81,4 +82,38 @@ router.post("/profile",fileuploads.diskLoader.single("file"),async (req,res)=>{
             });
         }
     });
+});
+router.put("/:id",async (req,res)=>{
+    const id = req.params.id;
+ const body:UserPostResp = req.body;
+ let sql = "select * from user where uid = ?";
+ sql = mysql.format(sql,[ id ]);
+ const result = await queryAsync(sql);
+ const json = JSON.stringify(result);
+ const obj = JSON.parse(json);
+
+ let original :UserPostResp | undefined;
+ original = obj[0] as UserPostResp;
+
+ let update = {...original, ...body};
+  sql = "UPDATE `user` SET `name`= ?, `type`= ?, `email`= ?, `pass`= ? ,`last_name` = ?  where `uid` = ?";
+
+ sql = mysql.format(sql,[
+    update.name,
+    update.type,
+    update.email,
+    update.pass,
+    update.last_name,
+    id
+ ]);
+ conn.query(sql,(err,result)=>{
+    if(err){
+        res.json(err);
+    }else{
+        res.status(200).json({
+            affected_row : result.affected_row,
+            "update":"fulll !!!"
+        });
+    }
+ });
 });
